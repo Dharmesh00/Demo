6d59f291596e5e1d6d2b2a0fe19270065ac3f680
Dharmesh A.S <dharmesh.sridharmesh@gmail.com>
2026-09-19 05:06:10 +0530

    Gravater Added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
-#from flask_gravatar import Gravatar
+from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects import sqlite
@@ -48,7 +48,7 @@ def load_user(user_id):
 
 # For adding profile images to the comment section
 
-"""
+
 gravatar = Gravatar(app,
                     size=100,
                     rating='g',
@@ -57,7 +57,7 @@ gravatar = Gravatar(app,
                     force_lower=False,
                     use_ssl=False,
                     base_url=None)
-"""
+
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
